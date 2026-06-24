b9344dd9f5a7b1e2606c854658c80640178cc5f6
Thorsten Knoll <thorsten_knoll@yahoo.de>
2026-06-24 12:15:51 +0200

    synth gds v0.1

--- src/chip_core.sv
+++ src/chip_core.sv
@@ -20,13 +20,18 @@ module chip_core #(
     inout  wire [NUM_ANALOG_PADS-1:0] analog      // Analog
 );
 
-    // Set all bidir as output
-    assign bidir_oe = '1;
-
+    //--------------------
+    // Bidircetional pins 
+    //--------------------
     logic _unused;
     assign _unused = &bidir_in;
+    assign bidir_out = '0;
+    assign bidir_oe = '1;
 
-    logic [NUM_BIDIR_PADS-1:0] count;
+    //--------------------
+    // Output pins (counter) 
+    //--------------------
+    logic [NUM_OUTPUT_PADS-1:0] count;
 
     always_ff @(posedge clk) begin
         if (!rst_n) begin
@@ -39,56 +44,6 @@ module chip_core #(
     end
     
     assign output_out = count;
-
-    logic [31:0] sram_0_out;
-
-    RM_IHPSG13_1P_1024x32_c2_bm_bist sram_0 (
-        .A_CLK  (clk),
-        .A_MEN  (1'b1),
-        .A_WEN  (1'b0),
-        .A_REN  (1'b1),
-        .A_ADDR ('0),
-        .A_DIN  ('0),
-        .A_DLY  (1'b1), // tie high!
-        .A_DOUT (sram_0_out),
-        .A_BM   ('0),
-        
-        // Built-in self test port
-        .A_BIST_CLK   ('0),
-        .A_BIST_EN    ('0),
-        .A_BIST_MEN   ('0),
-        .A_BIST_WEN   ('0),
-        .A_BIST_REN   ('0),
-        .A_BIST_ADDR  ('0),
-        .A_BIST_DIN   ('0),
-        .A_BIST_BM    ('0)
-    );
-
-    logic [31:0] sram_1_out;
-
-    RM_IHPSG13_1P_1024x32_c2_bm_bist sram_1 (
-        .A_CLK  (clk),
-        .A_MEN  (1'b1),
-        .A_WEN  (1'b0),
-        .A_REN  (1'b1),
-        .A_ADDR ('0),
-        .A_DIN  ('0),
-        .A_DLY  (1'b1), // tie high!
-        .A_DOUT (sram_1_out),
-        .A_BM   ('0),
-        
-        // Built-in self test port
-        .A_BIST_CLK   ('0),
-        .A_BIST_EN    ('0),
-        .A_BIST_MEN   ('0),
-        .A_BIST_WEN   ('0),
-        .A_BIST_REN   ('0),
-        .A_BIST_ADDR  ('0),
-        .A_BIST_DIN   ('0),
-        .A_BIST_BM    ('0)
-    );
-
-    assign bidir_out = {7'b0, (^sram_0_out) ^ (^sram_1_out)};
 
 endmodule
 
